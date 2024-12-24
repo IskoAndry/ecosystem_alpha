313bb188e7182f73cc28d190aa8b43df0d44f9ec
isakov <isakov4852@mail.ru>
2024-12-24 20:29:10 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/products/:id/edit" element={<EditProductPage />} />
-        <Route path="/products/:id" element={<ProductDetails/>} />
-        <Route path="/create-product" element={<CreateProductPage/>} />
-        <Route path="/" element={<ProductsPage/>} />
+        <Route path="/ecosystem_alpha/products/:id/edit" element={<EditProductPage />} />
+        <Route path="/ecosystem_alpha/products/:id" element={<ProductDetails/>} />
+        <Route path="/ecosystem_alpha/create-product" element={<CreateProductPage/>} />
+        <Route path="/ecosystem_alpha/" element={<ProductsPage/>} />
       </Routes>
     </Router>
   );
